a93a02adc6ea0d11131f77b46180de8168b19308
Martijn Wallage <67522397+MartijnWallage@users.noreply.github.com>
2023-05-19 14:57:24 +0200

    Create ft_printf.h

---
+++ includes/ft_printf.h
@@ -0,0 +1,21 @@
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+typedef struct s_print                       
+{                        
+      va_list  args;    # arg to print out                    
+      int   wdt;        # width                
+      int   prc;        # precision              
+      int   zero;       # zero padding               
+      int   pnt;        # .     
+      int   dash;       # -               
+      int   tl;         # total_length (return value)              
+      int   sign;       # pos or neg number               
+      int   is_zero;    # is number zero                 
+      int   perc;       # %               
+      int   sp;         # space flag ' '            
+}    t_print;
+
+int ft_printf(const char *format, ...);
+
+#endif
