6a9f4fe9c964b910435b493331bf4a21ca269026
Martijn Wallage <67522397+MartijnWallage@users.noreply.github.com>
2023-05-19 14:48:45 +0200

    Create ft_printf.c

---
+++ includes/ft_printf.c
@@ -0,0 +1,12 @@
+int ft_printf(const char *format, ...)
+{
+  int   return_value;
+  t_print *tab;
+  
+  return_value = 0;
+  tab = malloc(sizeof(t_print));
+  if (tab == NULL)
+    return (0);
+  init_tab(tab);
+  return (return_value);
+}
