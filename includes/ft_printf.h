9cdf03acdd77850572ba1d1a489ee520cd9ea41e
Martijn Wallage <mwallage@c4b4c3.42berlin.de>
2023-05-24 15:58:07 +0200

    Norminette type stuff

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -5,20 +5,20 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-typedef struct s_print                       
-{                        
-      va_list  args;    # arg to print out                    
-      int   wdt;        # width                
-      int   prc;        # precision              
-      int   zero;       # zero padding               
-      int   pnt;        # .     
-      int   dash;       # -               
-      int   tl;         # total_length (return value)              
-      int   sign;       # pos or neg number               
-      int   is_zero;    # is number zero                 
-      int   perc;       # %               
-      int   sp;         # space flag ' '            
-}    t_print;
+typedef struct	s_print                       
+{
+	va_list	args;
+	int		width;
+	int		precision;
+	int		zero;
+	int		point;
+	int		dash;
+	int		total_len;
+	int		sign;
+	int		is_zero;
+	int		percent;
+	int		space;
+}				t_print;
 
 int ft_printf(const char *format, ...);
 
--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -1,43 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mwallage <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/24 13:07:39 by mwallage          #+#    #+#             */
+/*   Updated: 2023/05/24 14:50:20 by mwallage         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
 
-void  ft_init_tab(t_print *tab)
+void	ft_init_tab(t_print *tab)
 {
-  tab->wdt = 0;        # we set everything to 0, false        
-  tab->prc = 0;                        
-  tab->zero = 0;                        
-  tab->pnt = 0;                        
-  tab->sign = 0;                        
-  tab->tl = 0;                        
-  tab->is_zero = 0;                        
-  tab->dash = 0;                        
-  tab->perc = 0;                        
-  tab->sp = 0;                        
-  return (tab);                    
+	tab->width		= 0;
+	tab->precision	= 0;
+	tab->zero		= 0;
+	tab->point		= 0;
+	tab->dash		= 0;
+	tab->total_len	= 0;
+	tab->sign		= 0;
+	tab->is_zero	= 0;
+	tab->percent	= 0;
+	tab->space		= 0;
+	return (tab);
 }
 
-int ft_printf(const char *format, ...)
+int	ft_printf(const char *format, ...)
 {
-  int   ret;
-  int   i;
-  t_print *tab;
-  
-  return_value = 0;
-  tab = malloc(sizeof(t_print));
-  if (tab == NULL)
-    return (-1);
-  ft_init_tab(tab);
-  va_start(tab->args, format);
-  i = -1;
-  ret = 0;
-  while (format[++i])
-  {
-    if (format[i] == '%')
-      i = ft_eval_format(tab, format, i + 1)
-    else
-      ret += write(1, &format[i], 1); 
-  }
-  va_end(tab->args);
-  ret += tab->tl;
-  free(tab);
-  return (ret);
+	int		return_value;
+	int		i;
+	t_print	*tab;
+
+	return_value = 0;
+	tab = malloc(sizeof(t_print));
+	if (tab == NULL)
+		return (-1);
+	ft_init_tab(tab);
+	va_start(tab->args, format);
+	i = -1;
+	return_value = 0;
+	while (format[++i])
+	{
+		if (format[i] == '%')
+			i = ft_eval_format(tab, format, i + 1)
+		else
+			return_value += write(1, &format[i], 1);
+	}
+	va_end(tab->args);
+	return_value += tab->total_len;
+	free(tab);
+	return (return_value);
 }
